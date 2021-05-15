9867beabedb87b67fa33e3d7eedf34201d9bc061
Oluwashina <shinzbaba@gmail.com>
2021-05-15 18:07:42 +0100

    next project start

---
+++ pages/about.js
@@ -0,0 +1,18 @@
+import Head from "next/head";
+
+const About = () => {
+    return ( 
+        <>
+        <Head>
+            <title>Ninja List | About</title>
+            <meta name="keywords" content="ninjas"></meta>
+        </Head>
+        <div>
+            <h1>About Page</h1>
+            <p>lorem ipsum</p>
+        </div>
+        </>
+     );
+}
+ 
+export default About;
